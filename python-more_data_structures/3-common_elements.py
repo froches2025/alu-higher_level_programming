63a518d2ce3e13d4c0c7258e6ae72403db4c23b1
froches2025 <f.michael@alustudent.com>
2026-04-17 13:31:49 +0200

    elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    commonWords = set()
+    for words in set_1:
+        if words in set_2:
+            commonWords.add(words)
+    return commonWords
+
+# alternative...this short ways are killing me🤯😂:
+# def common_elements(set_1, set_2):
+# return set_1 & set_2
